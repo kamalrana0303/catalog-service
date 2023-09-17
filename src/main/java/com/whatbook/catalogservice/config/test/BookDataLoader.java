f1598bd3ce8bdc13eacb08456a6b1214de85fc0f
kamalrana03 <kamalrana03@gmail.com>
2023-09-17 13:10:21 +0530

    added slf4j logger for logging

--- src/main/java/com/whatbook/catalogservice/config/test/BookDataLoader.java
+++ src/main/java/com/whatbook/catalogservice/config/test/BookDataLoader.java
@@ -2,15 +2,17 @@ package com.whatbook.catalogservice.config.test;
 
 import com.whatbook.catalogservice.BookRepository;
 import com.whatbook.catalogservice.entities.Book;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
+import org.slf4j.Logger;
 @Component
 @Profile("test")
 public class BookDataLoader {
     private final BookRepository bookRepository;
+    private static final Logger logger = LoggerFactory.getLogger(BookDataLoader.class);
 
     public BookDataLoader(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -22,6 +24,8 @@ public class BookDataLoader {
         var book2 =  Book.of("1234567892", "Polar Journey",
                 "Iorek Polarson", 12.90);
         bookRepository.save(book1);
+        logger.info(book1.toString());
         bookRepository.save(book2);
+        logger.info(book2.toString());
     }
 }
