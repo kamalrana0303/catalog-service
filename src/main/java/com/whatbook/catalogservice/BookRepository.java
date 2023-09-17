b868b66532cc9e49b552b3933579b54b1e555759
kamalrana03 <kamalrana03@gmail.com>
2023-09-17 12:55:22 +0530

    use query

--- src/main/java/com/whatbook/catalogservice/BookRepository.java
+++ src/main/java/com/whatbook/catalogservice/BookRepository.java
@@ -1,14 +1,22 @@
 package com.whatbook.catalogservice;
 
 import com.whatbook.catalogservice.entities.Book;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book,Long> {
-    Optional<Book> findByIsbn(String isbn);
+    @Query("select b from Book b  where b.isbn =:isbn")
+    Optional<Book> findByIsbn(@Param("isbn") String isbn);
+    @Query("select b from Book b")
     Iterable<Book> findAll();
-    boolean existsByIsbn(String isbn);
+    @Query("SELECT COUNT(b) > 0 FROM Book b WHERE b.isbn =:isbn")
+    boolean existsByIsbn(@Param("isbn") String isbn);
     Book save(Book book);
-    void deleteByIsbn(String isbn);
+    @Modifying
+    @Query("delete from Book b where b.isbn =:isbn")
+    void deleteByIsbn(@Param("isbn") String isbn);
 }
